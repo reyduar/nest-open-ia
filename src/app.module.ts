b7ad014a1af60634fc3b2c68cb3c981ccfae33e7
“ariel.duarte” <arielpy@gmail.com>
2024-10-09 17:49:03 -0300

    Added procfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GptModule } from './gpt/gpt.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GptModule],
+  imports: [ConfigModule.forRoot(), GptModule, HealthModule],
   controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   app.enableCors();
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
