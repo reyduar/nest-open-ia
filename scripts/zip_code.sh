eb297cfa109fdb0c5db18b4b0e47023c3eef31eb
“ariel.duarte” <arielpy@gmail.com>
2024-10-09 22:06:48 -0300

    Added circle ci config to deploy api on AWS EB

--- scripts/zip_code.sh
+++ scripts/zip_code.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Nombre del archivo ZIP
+ZIP_FILE="damc-api-open-ia.zip"
+
+# Comando para crear el archivo ZIP e incluir todos los archivos, incluidos los ocultos
 cd ..
-zip -r nest-open-ia.zip . -x "node_modules/*" -x "dist/*"
+zip -r $ZIP_FILE . -x "node_modules/*" -x "dist/*"  -x "generated/*" -x "scripts/*"
+
+# Mensaje de confirmación
+echo "File $ZIP_FILE has been successfully created."
