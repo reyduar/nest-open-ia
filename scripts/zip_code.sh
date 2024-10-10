d76aa0f904329bb4ed8f0f669413c809c5fe0a5a
“ariel.duarte” <arielpy@gmail.com>
2024-10-09 22:46:37 -0300

    Updated zip_code.sh file

--- scripts/zip_code.sh
+++ scripts/zip_code.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Nombre del archivo ZIP
 ZIP_FILE="damc-api-open-ia.zip"
 
-# Comando para crear el archivo ZIP e incluir todos los archivos, incluidos los ocultos
 cd ..
 zip -r $ZIP_FILE . -x "node_modules/*" -x "dist/*"  -x "generated/*" -x "scripts/*"
 
-# Mensaje de confirmación
 echo "File $ZIP_FILE has been successfully created."
